d02807719bd664243adbb1b9e35d849dd22ef524
Đỗ Lâm Thành Phát <dltphat301@gmail.com>
2025-08-28 08:47:50 +0700

    Update user.service.ts
    Lưu ý service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -7,7 +7,9 @@ import omit from 'lodash/omit'
 import MSG from '../constants/messages'
 import { sign } from 'crypto'
 import { signToken } from '@/utils/jwt'
-
+// Những chỗ này thì không có gì phải sửa
+// Nhưng có 1 số kiến thức a nghĩ là em nên note lại để ôn lại tí xíu
+// phân phân rả 1 object hoặc là lưu lại object như thế nào và lưu array như thế nào cái này a sẽ cùng cấp tài liệu trên nhóm của mình em nha. Nó là mảng và object
 interface SignAccessTokenServicePayload {
   user_id: string
 }
