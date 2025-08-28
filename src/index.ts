406bcfdceb8821e2a4f23d1629340125dc8f1ac5
Đỗ Lâm Thành Phát <dltphat301@gmail.com>
2025-08-28 08:35:13 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { CONFIG_ENV } from './constants/config';
 import { PREFIX_API } from './constants/path';
 import userRouter from './routes/user.route'
 
+// Thứ nhất là ngay chỗ này cú pháp process.env.PORT không sai. Nhưng em dùng như này sẽ bị lỗi. Và phải dùng cú pháp config() để có thể dùng được tất cả OBJECT bên trong process
+// Thứ hai là lý do nên dùng constants/config.ts để có thể gọi 1 lần duy nhất config() - Tránh gọi đi gọi lại nhiều lần. Còn em đang gọi trực tiếp như này là hiệu năng code sẽ bị chậm đi rất nhiều
 const port = process.env.PORT || 9000;
 
 const app = express();
